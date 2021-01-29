b3a44cbd6aef50a350b57730ce3b04c646e00fee
comrade77 <nimec77@gmail.com>
2021-01-29 15:41:03 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:equatable/equatable.dart';
+import 'package:example/authentication_flow/authentication_flow.dart';
+import 'package:example/location_flow/location_flow.dart';
+import 'package:example/onboarding_flow/onboarding_flow.dart';
+import 'package:example/profile_flow/profile_flow.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  EquatableConfig.stringify = kDebugMode;
+  runApp(MyApp(locationRepository: LocationRepository()));
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key key, @required LocationRepository locationRepository})
+      : assert(locationRepository != null),
+        _locationRepository = locationRepository,
+        super(key: key);
+
+  final LocationRepository _locationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _locationRepository,
+      child: MaterialApp(home: Home()),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Builder(
+        builder: (context) {
+          return ListView(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.help_outline),
+                title: const Text('Onboarding Flow'),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () async {
+                  await Navigator.of(context).push(OnboardingFlow.route());
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Onboarding Flow Complete!'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.person_outline),
+                title: const Text('Profile Flow'),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () async {
+                  final profile = await Navigator.of(context).push(
+                    ProfileFlow.route(),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Profile Flow Complete!\n$profile'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.location_city),
+                title: const Text('Location Flow'),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () async {
+                  final location = await Navigator.of(context).push(
+                    LocationFlow.route(),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Location Flow Complete!\n$location'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.security_rounded),
+                title: const Text('Authentication Flow'),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () async {
+                  await Navigator.of(context).push<AuthenticationState>(
+                    AuthenticationFlow.route(),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Authentication Flow Complete!'),
+                    ),
+                  );
+                },
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/onboarding_flow/onboarding_flow.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flow_builder/flow_builder.dart';
+
+List<Page> onGenerateOnboardingPages(OnboardingSteps state, List<Page> pages) {
+  switch (state) {
+    case OnboardingSteps.step1:
+      return [Step1.page()];
+    case OnboardingSteps.step2:
+      return [Step1.page(), Step2.page()];
+    case OnboardingSteps.step3:
+      return [Step1.page(), Step2.page(), Step3.page()];
+    default:
+      return [Step1.page()];
+  }
+}
+
+enum OnboardingSteps { step1, step2, step3 }
+
+class OnboardingFlow extends StatelessWidget {
+  static Route<void> route() {
+    return MaterialPageRoute(builder: (_) => OnboardingFlow());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const FlowBuilder<OnboardingSteps>(
+      state: OnboardingSteps.step1,
+      onGeneratePages: onGenerateOnboardingPages,
+    );
+  }
+}
+
+class Step1 extends StatelessWidget {
+  static MaterialPage<void> page() => MaterialPage<void>(child: Step1());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () => context.flow<OnboardingSteps>().complete(),
+        ),
+      ),
+      body: const Center(child: Text('Onboarding Step 1')),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 0,
+            onPressed: () {
+              context.flow<OnboardingSteps>().complete((_) => null);
+            },
+            child: const Icon(Icons.clear),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            heroTag: 1,
+            onPressed: () {
+              context
+                  .flow<OnboardingSteps>()
+                  .update((previous) => OnboardingSteps.step2);
+            },
+            child: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Step2 extends StatelessWidget {
+  static MaterialPage<void> page() => MaterialPage<void>(child: Step2());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text('Onboarding Step 2')),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 2,
+            onPressed: () {
+              context
+                  .flow<OnboardingSteps>()
+                  .update((_) => OnboardingSteps.step1);
+            },
+            child: const Icon(Icons.arrow_back_ios_rounded),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            heroTag: 3,
+            onPressed: () {
+              context
+                  .flow<OnboardingSteps>()
+                  .update((_) => OnboardingSteps.step3);
+            },
+            child: const Icon(Icons.arrow_forward_ios_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Step3 extends StatelessWidget {
+  static MaterialPage<void> page() => MaterialPage<void>(child: Step3());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(child: Text('Onboarding Step 3')),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 4,
+            onPressed: () {
+              context
+                  .flow<OnboardingSteps>()
+                  .update((_) => OnboardingSteps.step2);
+            },
+            child: const Icon(Icons.arrow_back_ios_rounded),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            heroTag: 5,
+            onPressed: () {
+              context.flow<OnboardingSteps>().complete((_) => null);
+            },
+            child: const Icon(Icons.check),
+          ),
+        ],
+      ),
+    );
+  }
+}
